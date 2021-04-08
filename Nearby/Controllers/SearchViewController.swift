d5d36593f0c3a10ed36ad8f5114014c157a9662b
Taguchi-wq <20cm0123@jec.ac.jp>
2021-04-08 17:16:12 +0900

    検索画面の枠組みを作成

--- Nearby/Controllers/SearchViewController.swift
+++ Nearby/Controllers/SearchViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  SearchViewController.swift
 //  Nearby
 //
 //  Created by cmStudent on 2021/04/08.
@@ -7,13 +7,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class SearchViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
-
 }
-
